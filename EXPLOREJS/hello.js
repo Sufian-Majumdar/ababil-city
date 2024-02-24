bb3494dc7e7548baa03b34ef0af6b874aff78a7f
Sufian <sufianmasumdar@gmail.com>
2024-02-24 09:25:33 +0600

    first commit

---
+++ EXPLOREJS/hello.js
@@ -0,0 +1,8 @@
+console.log(48)
+console.log( typeof name);
+var isHot = true;
+console.log(typeof isHot);
+var fastName = 'Md';
+console.log(fastName);
+// javascript keyword search
+ 
---
+++ Procoder/app.js
@@ -0,0 +1,21 @@
+// it is single line comment
+/* It is multi line  comment.
+CSS and javaScript comment are same
+*/
+document.write('Hello Me');
+//Variable
+var x = 10;
+var y = 20;
+var z = x+y;
+document.write(z);
+var bananaPrice = 8;
+document.write(bananaPrice);
+var name = 'Sufian Majumdar';
+var firstName = 'Sufian';
+console.log(name);
+document.write(name)
+console.log(firstName);
+var isHot = true;
+var isRich = false;
+console.log(isHot);
+document.write(typeofisHot);
